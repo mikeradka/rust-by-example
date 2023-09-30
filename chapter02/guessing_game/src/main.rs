e1eb2ad4b4e88ade3c39c711b89db61b7f69f5a3
mikeradka <mikeradka@gmail.com>
2023-09-30 17:40:38 -0400

    Read input

--- chapter02/guessing_game/src/main.rs
+++ chapter02/guessing_game/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io; // Bring the std::io library into scope
+
+// The main function is the entry point into the program
+fn main() { // Declare the main function with no parameters
+    println!("Guess the number!");  // println! is a marcro that prints a string to the screen
+    println!("Please input your guess: ");
+
+    let mut guess = String::new();  // Create a new mutable empty string
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
