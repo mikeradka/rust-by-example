229b8d6ac787906dfacad61df943963cc34bc32b
mikeradka <mikeradka@gmail.com>
2023-09-30 19:40:50 -0400

    Add mutability example

--- chapter03/variables/src/main.rs
+++ chapter03/variables/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    /* 
+     * In Rust, variables are immutable by default
+     * 
+     * let x = 5;
+     * println!("The value of x is {x}");
+     * x = 6;
+     * println!("The value of x is: {x}");
+     * 
+     * This would result in a compiling error
+     * x = 6;
+     * ^^^^^ cannot assign twice to immutable variable
+     */
+
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
