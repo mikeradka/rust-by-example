4c473e311de67a95ef87edbda1d381978e2192d9
mikeradka <mikeradka@gmail.com>
2023-09-30 18:22:02 -0400

    Generate a secret number

--- chapter02/guessing_game/src/main.rs
+++ chapter02/guessing_game/src/main.rs
@@ -1,11 +1,20 @@
 use std::io; // Bring the std::io library into scope
+use rand::Rng;  // Bring the Rng trait into scope
 
 // The main function is the entry point into the program
 fn main() { // Declare the main function with no parameters
     println!("Guess the number!");  // println! is a marcro that prints a string to the screen
+
+    // Generate a secret number between 1 and 100
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // Print the secret number
+    println!("The secret number is {secret_number}");
+
     println!("Please input your guess: ");
 
-    let mut guess = String::new();  // Create a new mutable empty string
+    // Create a new mutable empty string
+    let mut guess = String::new();
 
     // Receiving user input
     io::stdin()
