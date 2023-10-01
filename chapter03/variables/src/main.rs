f747e6e72dff9806460f90c68a65961b4cb9721f
mikeradka <mikeradka@gmail.com>
2023-09-30 20:07:57 -0400

    Add constant

--- chapter03/variables/src/main.rs
+++ chapter03/variables/src/main.rs
@@ -13,7 +13,19 @@ fn main() {
      */
 
     let mut x = 5;
-    println!("The value of x is: {x}");
+    println!("The first value of mutable x is: {x}");
     x = 6;
-    println!("The value of x is: {x}");
+    println!("The second value of mutable x is: {x}");
+    
+    /*
+     * Constants aren't just immutable by default,
+     * they're always immutable. They are declared
+     * by using the let keyword and the type of value
+     * must be annotated.
+     * Constants may be set only to a constant expression,
+     * not the result of a value that can only be
+     * computed at runtime.
+     */
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Constant THREE_HOURS_IN_SECONDS: {THREE_HOURS_IN_SECONDS}");
 }
