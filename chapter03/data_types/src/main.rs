422fa0ccdaeffd36d608391c27a0467e9490dc17
mikeradka <mikeradka@gmail.com>
2023-10-02 19:40:48 -0400

    Add some examples

--- chapter03/data_types/src/main.rs
+++ chapter03/data_types/src/main.rs
@@ -18,4 +18,22 @@ fn main() {
      */
     let guess: u32 = "42".parse().expect("Not a number!");
     println!("guess = {guess}");
+
+    let my_decimal_1 = 98221;
+    println!("my_decimal_1 = {my_decimal_1} (i32)");
+
+    let my_decimal_2 = 98_222;  // the underscore can be used to more easily read
+    println!("my_decimal_2 = {my_decimal_2} (i32)");
+
+    let my_hex = 0xff;  // hex 0xff = 255
+    println!("my_hex = {my_hex} (i32)");
+
+    let my_octal = 0o77; // 63
+    println!("my_octal = {my_octal} (i32)");
+
+    let my_binary = 0b1111_0000; // 240
+    println!("my_binary = {my_binary} (i32)");
+
+    let my_byte = b'A';
+    println!("my_byte = {my_byte} (u8)");
 }
