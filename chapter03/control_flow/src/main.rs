f4d599db5d922a0af5333ec3a5898c33274def29
mikeradka <mikeradka@gmail.com>
2023-10-16 17:15:17 -0400

    add two test conditions

--- chapter03/control_flow/src/main.rs
+++ chapter03/control_flow/src/main.rs
@@ -1,10 +1,21 @@
 fn main() {
-    let number = 7;
+    // Test 1
+    let number = 3;
   
     if number < 5 {
       println!("condition was true");
     }
     else {
       println!("condition was false");
+    }
+
+    // Test 2
+    let number2 = 7;
+
+    if number2 < 5 {
+        println!("condition was true");
+    }
+    else {
+        println!("condition was false");
     }
 }
