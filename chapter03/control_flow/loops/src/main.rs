a7c708e04d374bb9bcdc2159bdc487d3757cc647
mikeradka <mikeradka@gmail.com>
2023-11-01 21:37:23 -0400

    returning values from loops

--- chapter03/control_flow/loops/src/main.rs
+++ chapter03/control_flow/loops/src/main.rs
@@ -4,4 +4,17 @@ fn main() {
         println!("again!");
     }
     */
+
+    // Returning values from loops
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}.");
 }
